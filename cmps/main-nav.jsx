5f372c92ed8ee814a5796108a920ec254f07857c
Shay Skitel <61014430+ShaySkitel@users.noreply.github.com>
2022-12-26 15:09:51 +0200

    Change main navigation to use react router & add routes

--- cmps/main-nav.jsx
+++ cmps/main-nav.jsx
@@ -1,10 +1,12 @@
+const {NavLink} = ReactRouterDOM
+
 export function MainNav() {
     return <nav className="main-nav flex justify-between align-center">
         <h1>Miss Book</h1>
         <ul className="main-nav clean-list flex">
-            <li>Home</li>
-            <li>About</li>
-            <li>Books</li>
+            <li><NavLink to='/'>Home</NavLink></li>
+            <li><NavLink to='/about'>About</NavLink></li>
+            <li><NavLink to='book'>Books</NavLink></li>
         </ul>
     </nav>
 }
--- root-cmp.jsx
+++ root-cmp.jsx
@@ -1,4 +1,5 @@
-const { useState } = React
+const Router = ReactRouterDOM.HashRouter
+const { Route, Routes } = ReactRouterDOM
 
 import { MainNav } from './cmps/main-nav.jsx'
 import { About } from './views/about.jsx'
@@ -6,17 +7,18 @@ import { BookIndex } from './views/book-index.jsx'
 import { Home } from './views/home.jsx'
 
 export function App() {
-
-    const [page, setPage] = useState('books')
-
-    return <section className="app main-layout">
-        <header className="app-header">
-            <MainNav />
-        </header>
-        <main>
-            {page === 'home' && <Home />}
-            {page === 'about' && <About />}
-            {page === 'books' && <BookIndex />}
-        </main>
-    </section>
+    return <Router>
+        <section className="app main-layout">
+            <header className="app-header">
+                <MainNav />
+            </header>
+            <main>
+                <Routes>
+                    <Route element={<Home />} path='/'></Route>
+                    <Route element={<About />} path='/about'></Route>
+                    <Route element={<BookIndex />} path='/book'></Route>
+                </Routes>
+            </main>
+        </section>
+    </Router>
 }
